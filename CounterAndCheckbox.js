e0250db252b5ede7ae03f10e8738d17a5662d484
chibbi <68948051+chibbi@users.noreply.github.com>
2020-11-08 01:45:09 +0100

    Update CounterAndCheckbox.js

--- CounterAndCheckbox.js
+++ CounterAndCheckbox.js
@@ -1,120 +1,73 @@
-var expanded = false;
+<!doctype html>
 
-function showCheckboxes() {
-    var checkboxes = document.getElementById("checkboxes");
-    if (!expanded) {
-        checkboxes.style.display = "block";
-        expanded = true;
-    } else {
-        checkboxes.style.display = "none";
-        expanded = false;
-    }
-}
+<html lang="en">
 
-function loadCheckboxes() {
-    var cookies = document.cookie;
-    console.log(cookies);
-    console.log(cookies.split(" "));
-    // this section loads every cookie for every timer
-    loadCheckbox("vid");
-    loadCheckbox("viw");
-    loadCheckbox("viph");
-    loadCheckbox("vie");
-    loadCheckbox("vich");
-    loadCheckbox("vim");
-    // finished loading
-}
+<head>
+  <meta charset="UTF-8">
+  
+  <title>Abi Countdown</title>
+  <link rel="stylesheet" href="appereance.css">
+  <meta name="description" content="Official Website of Raninninn">
+  <meta name="author" content="chibbi">
+  <script data-ad-client='ca-pub-9021480086144976' async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script>
+</head>
 
-function loadCheckbox(visibilityBox) {
-    // basically searches the cookie string after the given name and if it is
-    // false
-    if (document.cookie.includes("checkBox_" + visibilityBox + "=false")) {
-        document.getElementById(visibilityBox).checked = false;
-        console.log(visibilityBox + " is FALSE");
-    // or true
-    } else if (document.cookie.includes("checkBox_" + visibilityBox + "=true")) {
-        document.getElementById(visibilityBox).checked = true;
-        console.log(visibilityBox + " IS true");
-    // if it is neither false nor true is is made true by default
-    } else {
-        document.getElementById(visibilityBox).checked = true;
-        console.log(visibilityBox + " MADE true");
-    }
-}
+<body onload="loadCheckboxes()">
+  <script src="CounterAndCheckbox.js"></script>
+  <div class="navBar">
+    <ul id="navBar">
+    </ul>
+  </div>
+  
+  <div class="mainContent">
+    <div class="multiselect">
+      <div class="selectBox" onclick="showCheckboxes()">
+        <select>
+          <option>Wähle sichtbare Prüfungen aus</option>
+        </select>
+        <div class="overSelect"></div>
+      </div>
+      <div id="checkboxes">
+        <label for="one">
+          <input type="checkbox" id="vid" />Deutsch</label>
+        <label for="two">
+          <input type="checkbox" id="viph" />Physik</label>
+        <label for="three">
+          <input type="checkbox" id="viw" />Wirtschaft</label>
+        <label for="three">
+          <input type="checkbox" id="vie" />Englisch</label>
+        <label for="three">
+          <input type="checkbox" id="vich" />Chemie</label>
+        <label for="three">
+          <input type="checkbox" id="vim" />Mathe</label>
+      </div>
+    </div>
+    <div class="cdc" id="dc">
+      <p class="cdt">Deutsch Prüfung in:</p>
+      <p class="cd" id="d"></p>
+    </div>
+    <div class="cdc" id="phc">
+      <p class="cdt">Physik Prüfung in:</p>
+      <p class="cd" id="ph"></p>
+    </div>
+    <div class="cdc" id="wc">
+      <p class="cdt">Wirtschaft Prüfung in:</p>
+      <p class="cd" id="w"></p>
+    </div>
+    <div class="cdc" id="ec">
+      <p class="cdt">Englisch Prüfung in:</p>
+      <p class="cd" id="e"></p>
+    </div>
+    <div class="cdc" id="chc">
+      <p class="cdt">Chemie Prüfung in:</p>
+      <p class="cd" id="ch"></p>
+    </div>
+    <div class="cdc" id="mc">
+      <p class="cdt">Mathe Prüfung in:</p>
+      <p class="cd" id="m"></p>
+    </div>
+  </div>
+  <script src="siteLoader.js"></script>
+</body>
 
-function checkVisibility(counterID) {
-    // Update the count down every 1 second
-    var x = setInterval(function () {
-        // Checks if the given Text and Counter should be visible
-        if (document.getElementById("vi" + counterID).checked == true) {
-            // if yes, it will show them
-            document.getElementById(counterID + "c").style.display = "block";
-            document.cookie = "checkBox_" + "vi" + counterID + "=true;expires=Mon, 04 Jul 2022 22:44:25 UTC";
-        } else {
-            // if not, it will not show them
-            document.getElementById(counterID + "c").style.display = "none";
-            document.cookie = "checkBox_" + "vi" + counterID + "=false;expires=Mon, 04 Jul 2022 22:44:25 UTC";
-        }
-    }, 1000);
-}
-
-function countDown(countDownDate, counterID) {
-    // Update the count down every 1 second
-    var x = setInterval(function () {
-        // Get current date and time
-        var now = new Date().getTime();
-
-        var duration = countDownDate - now;
-
-        var months = Math.floor(duration / (1000 * 60 * 60 * 24 * 31));
-        var days = Math.floor((duration % (1000 * 60 * 60 * 24 * 31)) / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((duration % (1000 * 60)) / 1000);
-
-        // Output the result in an element with id=textID
-        document.getElementById(counterID).textContent =
-            months +
-            " Monate, " +
-            days +
-            " Tage, " +
-            hours +
-            " Stunden, " +
-            minutes +
-            " Minuten, " +
-            seconds +
-            " Sekunden ";
-
-        // If the countdown is over, show it
-        if (duration < 0) {
-            clearInterval(x);
-            document.getElementById(counterID).textContent = "Prüfungstart war bereits.";
-        }
-    }, 1000);
-}
-// basically checks if the given timer should be shown
-checkVisibility("d");
-checkVisibility("ph");
-checkVisibility("w");
-checkVisibility("e");
-checkVisibility("ch");
-checkVisibility("m");
-// creates the countdown for every timer
-countDown(new Date("May 4, 2021 9:00:00").getTime(), "d");
-countDown(new Date("May 6, 2021 9:00:00").getTime(), "ph");
-countDown(new Date("May 7, 2021 9:00:00").getTime(), "w");
-countDown(new Date("May 10, 2021 9:00:00").getTime(), "e");
-countDown(new Date("May 11, 2021 9:00:00").getTime(), "ch");
-countDown(new Date("May 17, 2021 9:00:00").getTime(), "m");
-
-
-// How to create new Dates:
-// 1. create new <p></p> Texts (with ids)
-// 2. create new Checkbox in DropDown Select Menu
-// 3. create new checkVisibility(...)
-// 4. create new countDown(...)
-// 5. create new loadCheckBox(...) in loadCheckBoxes()
-
-// How to change from one Abi to the next
-// 1. Change every Date for every Prüfung to the new Date
-// 2. Maybe change/increase ExpiringDate
+</html>
